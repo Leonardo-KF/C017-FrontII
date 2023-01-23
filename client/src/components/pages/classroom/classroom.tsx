c72820d1816dfcfce00b0becaafac2fe8135ce21
Leo <leonardo.fleck@waproject.com.br>
2023-01-23 19:45:06 -0300

    feat: add user data to localStorage

--- client/src/components/pages/classroom/classroom.tsx
+++ client/src/components/pages/classroom/classroom.tsx
@@ -20,6 +20,8 @@ export function Classroom() {
     (classroom) => classroom.id === selectedClassroom
   );
 
+  console.log(JSON.parse(localStorage.getItem("user") ?? "").role);
+
   async function findClassrooms() {
     const data = await api.getClassrooms();
     setClassrooms(data);
--- client/src/utils/api/api.ts
+++ client/src/utils/api/api.ts
@@ -46,6 +46,8 @@ export const api = {
         password,
       });
       localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+
       return response.data;
     } catch (err: any) {
       HandleError(err);
